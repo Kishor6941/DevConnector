2dc84bd077955f57e7af05dd13593cb94e64036f
Kishor6941 <phawadeks9049@gmail.com>
2020-09-12 22:49:29 +0530

    add delete post

---
+++ routes/Models/Post.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId, // to get the info of user using _id
+    ref: "user",
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+  },
+  avatar: {
+    type: String,
+  },
+  like: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users",
+      },
+    },
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users",
+      },
+      text: {
+        type: String,
+        required: true,
+      },
+      avatar: {
+        type: String,
+      },
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+});
+
+module.exports = Post = mongoose.model("post", PostSchema);
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,94 @@
 const express = require("express");
 const router = express.Router();
+const Post = require("../Models/Post");
+const User = require("../Models/User");
+const requireLogin = require("./Middleware/requireLogin");
+const { body, validationResult } = require("express-validator");
+
+// @route    POST api/posts
+// @desc    post the text
+//@access   Private
+router.post(
+  "/",
+  [requireLogin, [body("text", "Text is required").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+
+      const newpost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      });
+      const post = await newpost.save();
+      res.json(post);
+    } catch (error) {
+      console.error(error.message);
+      return res.status(500).send("Server error");
+    }
+  }
+);
 
 // @route    GET api/posts
-// @desc    Test route
-//@access   Public
-router.get("/", (req, res) => res.send("posts route"));
+// @desc    Get all the posts
+//@access   Private
+router.get("/", requireLogin, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).send("Server error");
+  }
+});
+
+// @route    GET api/posts/:id
+// @desc    Get posts by id
+//@access   Private
+router.get("/:id", requireLogin, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    res.json(post);
+  } catch (error) {
+    if (error.kind === "ObjectId") {
+      console.error(error.message);
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    return res.status(500).send("Server error");
+  }
+});
+
+// @route    DELETE api/posts/:id
+// @desc    Delete the post
+//@access   Private
+router.delete("/:id", requireLogin, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    // check user if this post own by that user or not
+    if (post.user.toString() !== req.user.id) {
+      // post.user is object which is conveted to string by using toString()
+      return res.status(401).json({ msg: "user not authorized" });
+    }
+    await post.remove();
+    res.json({ msg: "Post removed" });
+  } catch (error) {
+    if (error.kind === "ObjectId") {
+      console.error(error.message);
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    return res.status(500).send("Server error");
+  }
+});
 
 module.exports = router;
